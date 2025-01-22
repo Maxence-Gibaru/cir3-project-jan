37f1d0f2820db02ef99fcd25583160884f576b0f
Maxence-Gibaru <maxence.gibaru@stuident.junia.com>
2025-01-22 15:09:31 +0100

    update: fix login

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -138,9 +138,9 @@ export default function LoginForm() {
                     <button
                         type="submit"
                         className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-                    ><Link href="./create">
+                    >
                         S'inscrire
-                    </Link>
+
                     </button>
                 </form>
             </div>
