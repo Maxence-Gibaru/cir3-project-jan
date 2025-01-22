abc9cab9b3c767f0d9885bbc91616a57efa0dbc7
Paul MATHIEU <paul.mathieu@student.junia.com>
2025-01-22 18:23:25 +0100

    Cadeau

--- src/app/api/guest/check_is_started/route.ts
+++ src/app/api/guest/check_is_started/route.ts
@@ -2,9 +2,9 @@ import { authOptions } from "@/lib/authOptions";
 import dbConnect from "@/lib/dbConnect";
 import { HuntModel } from "@/models/Hunt";
 import { getServerSession } from "next-auth";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
+export async function GET() {
     const session = await getServerSession(authOptions);
     if (!session) return NextResponse.next({ status: 401 });
 
@@ -20,7 +20,8 @@ export async function GET(req: NextRequest) {
             );
         }
 
-        return NextResponse.json({ isStarted: hunt.status === "started" }, { status: 200 });
+        const isStarted = hunt.status === "started";
+        return NextResponse.json({ isStarted }, { status: 200 });
     } catch (error) {
         console.error(error);
         return NextResponse.json(
