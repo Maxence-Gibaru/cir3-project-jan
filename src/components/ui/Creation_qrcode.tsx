06e21ed15583aaeb4ff17947678dfe57e8da8acf
Jérémy LAMOUR <jeremy.lamour@student.junia.com>
2025-01-23 23:47:00 +0100

    modif jérémy

---
+++ src/components/ui/Creation_qrcode.tsx
@@ -0,0 +1,67 @@
+'use client'
+
+import React from 'react';
+import { jsPDF } from 'jspdf';
+import QRCode from 'qrcode';
+import { Hunt } from '@/models/Hunt';
+
+
+interface qr_codeProps {
+  hunt: Hunt;
+}
+export default function creation_qrcode({ hunt }: qr_codeProps) {
+  /* const [index, setIndex] = React.useState(1); */
+  const generatePDF = async () => {
+    const doc = new jsPDF();
+    const newMockData = hunt.markers.reduce((acc, marker, index) => {
+      acc[index + 1] = marker.id;
+      return acc;
+    }, {});
+
+    // Résultat potentiel :
+    // {
+    //   "1": "ecdfd631", 
+    //   "2": "da6fcfbe"
+    // }
+    console.log(newMockData);
+    const indices = Object.entries(newMockData);
+
+    for (let i = 0; i < indices.length; i++) {
+      const [key, value] = indices[i];
+
+      if (i > 0) {
+        doc.addPage();
+      }
+
+      doc.setFontSize(16);
+
+      if(i === 0) {
+        doc.text(`Trésor`, 30, 30);
+        } else {
+        doc.text(`Indice ${key}`, 30, 30);
+        }
+      try {
+        const qrCodeDataURL = await QRCode.toDataURL(value, {
+          width: 300,
+          margin: 2
+        });
+        doc.addImage(qrCodeDataURL, 'PNG', 30, 70, 150, 150);
+      } catch (err) {
+        console.error('Erreur QR code:', err);
+      }
+    }
+
+    doc.save('indices.pdf');
+  };
+
+  return (
+
+    <button
+      onClick={generatePDF}
+      className="bg-dark text-white px-6 py-3 rounded-lg hover:bg-blueBg"
+    >
+      Générer les QR codes des indices
+    </button>
+
+  );
+}
