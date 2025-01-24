94a91d919d6dd779bc1b044e4c3049222790806f
Paul MATHIEU <paul.mathieu@student.junia.com>
2025-01-24 01:49:00 +0100

    A l'aveugle

--- src/components/ui/Qrcode.tsx
+++ src/components/ui/Qrcode.tsx
@@ -3,7 +3,7 @@
 import Qr_code from "@/components/ui/scan_qr_code";
 import { fetchApi } from "@/lib/api";
 import { Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader } from "@heroui/react";
-import React, { memo } from 'react';
+import { memo } from 'react';
 interface QrcodeProps {
   isOpen: boolean;
   onOpenChange: () => void;
@@ -14,27 +14,20 @@ interface QrcodeProps {
 function  Qrcode({ isOpen, onOpenChange,lobbyCode}: QrcodeProps) {
 
   const onNewScanResult = async (decodedText: string) => {
-    try {
-      console.log(decodedText);
-
-      // Requête API avec le QR code
-      const response = await fetchApi('guest/qr_code', { 
-        method: 'GET', 
-        params: { lobby_code : lobbyCode, qr_code : decodedText }, 
-      });
+   
+    // Requête API avec le QR code
+    fetchApi('guest/qr_code', { 
+      method: 'GET', 
+      params: { lobby_code : lobbyCode, qr_code : decodedText }, 
+    }).then((response) => {
 
       console.log("response", response)
-  
-      // Gestion de la réponse
-      if (response.isCorrect === true) {
+      if (response.isCorrect) {
         onOpenChange()
-      } else if (response.isCorrect === false) {
-        alert("Mauvais QR-code");
       }
-    } catch (error) {
-      // Gestion des erreurs
-      console.error('Erreur lors de la requête :', error);
-    }
+    }).catch((error) => {
+      alert("Mauvais QR-code");
+    });
   };
   
 
